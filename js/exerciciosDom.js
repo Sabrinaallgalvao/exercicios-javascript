ef1b7cf238d4eebb629eb7df6425c31201f25678
Thalles Galvao <thallesgalv@gmail.com>
2022-03-19 12:31:11 -0300

    Exercicios 1 ao 6

--- js/exerciciosDom.js
+++ js/exerciciosDom.js
@@ -1,15 +1,55 @@
 // 1 - Faça uma função que altere a cor da fonte do primeiro parágrafo
+function ex1(cor) {
+  const paragrafo = document.querySelector('p')
+  paragrafo.style.color = cor
+}
+// ex1('red')
+
 
 // 2 - Faça uma função que altere a cor de fundo de todas as divs.
+function ex2(cor1) {
+  const divs = document.querySelectorAll('div')
+  for (item of divs) {
+    item.style.backgroundColor = cor1
+  }
+}
+//ex2('yellow')
 
 // 3 - Faça uma função que mude a cor azul de todos os elementos com a classe 'azul'
 
+function ex3() {
+  const elementos = document.querySelectorAll('.azul')
+  for (item of elementos) {
+    item.style.color = 'blue'
+  } 
+}
+//ex3()
 // 4 - Faça uma função que esconda o elemento com  id esconder.
 
+function ex4() {
+  //const elemento = document.querySelector('#esconder')
+  const elemento2 = document.getElementById('esconder')
+  elemento2.style.display = 'none'
+}
+//ex4()
 // 5 - Faça uma função que altere o título do documento para 'Alterando título"
+function ex5() {
 
+  // document.title = 'Alterando Tutulo' 
+  const titulo = document.querySelector('title')
+  titulo.innerHTML = 'Alterando Título'
+}
+// ex5()
 // 6 - Faça uma função que imprima no console a lista de classes do primeiro parágrafo do documento.
-
+function ex6() {
+  const paragrafo = document.querySelector('p')
+  const listaDeClasses = paragrafo.classList
+  
+  for (item of listaDeClasses) {
+    console.log(item)
+  }
+}
+// ex6()
 // 7 - Faça uma função que coloque um atributo alt e coloque o valor: "Imagem gerada aleatoriamente" na imagem que está no documento
 
 // 8 - Faça uma fução que selecione todas as tags p e div do documento e coloque uma borda de 1px, sólida e amarela nos itens pares da lista.
